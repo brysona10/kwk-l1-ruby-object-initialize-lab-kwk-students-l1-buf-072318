a90d54c5ead6a13cd84405aecadf56971fd6f63b
brysona10 <addisonbryson10@gmail.com>
2018-07-30 12:53:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,8 @@
 # dog.rb
 class Dog
-  attr_reader :name, :type, :breed 
+  attr_reader :name 
   def initialize(name, type, breed)
     @name = name
-    @type = type
-    @breed = breed
   end
 end
-fido = Dog.new("Fido", "Pug")
+fido = Dog.new("Fido")
